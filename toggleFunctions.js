6c72d3a93f160cbd4a3783c32e3204ae4336c4ee
Levistpatrick <42749570+Levistpatrick@users.noreply.github.com>
2019-03-08 14:00:17 -0600

    Add files via upload

---
+++ toggleFunctions.js
@@ -0,0 +1,40 @@
+function green25(){
+const doc = document 
+let divs = doc.querySelectorall(nav = a )
+body.classlist.add("green25");
+}
+
+
+function green50(){
+ const doc= document;
+ let divs = doc.queryselecteor("body")
+body.classlist.add("green50");
+}
+
+
+function green75(){
+
+
+}
+
+
+function green100(){
+
+
+}
+
+
+function smallToggle(){
+
+
+}
+
+
+function mediumToggle(){
+
+}
+
+
+function largeToggle(){
+
+}
